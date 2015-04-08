0dbbb625772dd07586b33fad3fee18eb096f1f02
StevoDuhHero <pgts2010@yahoo.com>
2015-04-07 21:31:21 -0700

    no is.not fuck that

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ io.on('connection', function(socket) {
   };
 
   socket.on('e', function(data) {
-    if (is.not.object(data) || is.not.propertyDefined(data, 'event')) return;
+		if (typeof data != "object" || !data.event) return;
     console.log(data.event);
     if (is.propertyDefined(events, data.event)) {
       events[data.event](user, data);
