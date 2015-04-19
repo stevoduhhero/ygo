5ff9960c7d9dc868eb9da21f59704ec0a5c8c6bb
StevoDuhHero <pgts2010@yahoo.com>
2015-04-18 23:24:17 -0700

    abc
    ddd

--- server.js
+++ server.js
@@ -32,10 +32,6 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// usernames which are currently connected to the chat
-var usernames = {};
-var numUsers = 0;
-
 /**
  * Socket connections.
  */
@@ -62,49 +58,8 @@ io.on('connection', function(socket) {
 		}
 	});
 
-    var addedUser = false;
-
-    // when the client emits 'new message', this listens and executes
-    socket.on('new message', function(data) {
-        // we tell the client to execute 'new message'
-        socket.broadcast.emit('new message', {
-            username: socket.username,
-            message: data
-        });
-    });
-
-    // when the client emits 'add user', this listens and executes
-    socket.on('add user', function(username) {
-        // we store the username in the socket session for this client
-        socket.username = username;
-        // add the client's username to the global list
-        usernames[username] = username;
-        ++numUsers;
-        addedUser = true;
-        socket.emit('login', {
-            numUsers: numUsers
-        });
-        // echo globally (all clients) that a person has connected
-        socket.broadcast.emit('user joined', {
-            username: socket.username,
-            numUsers: numUsers
-        });
-    });
-
-    // when the user disconnects.. perform this
     socket.on('disconnect', function() {
         console.log('a user disconnected');
-        // remove the username from global usernames list
-        if (addedUser) {
-            delete usernames[socket.username];
-            --numUsers;
-
-            // echo globally that this client has left
-            socket.broadcast.emit('user left', {
-                username: socket.username,
-                numUsers: numUsers
-            });
-        }
     });
 
 });
